1bd96f269d73cf2d686d7b86c74a79ffcb099640
Flynn Wang <freefeb@gmail.com>
2011-06-21 22:01:10 +0800

    add unexpired unviewed tips for user

--- controllers/todo.py
+++ controllers/todo.py
@@ -73,16 +73,23 @@ def merge():
     return _fetch_all_json()
   
 def _add_defaut_tips():
-    tip_counter = db.tip.id.count()
-    tip_count = db(db.tip.user_id==auth.user_id).select(tip_counter).first()(tip_counter)
-    print tip_count
+    tip_count = db(db.tip.user_id==auth.user_id).count()
     if tip_count == 0: 
         logger.log(32, 'add defaut tips for user %s' % auth.user.email)
-        default_tips = db(db.meta_tip.created < datetime(3,3,3)).select()
-        print default_tips
+        default_tips = db(db.meta_tip.expired_date < datetime(3,3,3)).select()
         db.tip.bulk_insert([{'meta_tip_id': t.id} for t in default_tips])
+
+def _add_new_tips():
+    query = (db.meta_tip.expired_date > datetime.now()) & (db.tip.user_id == auth.user_id) \
+                & (db.tip.meta_tip_id == db.meta_tip.id)
+    
+    added_tips = db(query).select(db.meta_tip.id)
+    unexpried_tips = db(db.meta_tip.expired_date > datetime.now()).select()
+    fresh_tips = filter(lambda t: not t in added_tips, unexpried_tips)
+    db.tip.bulk_insert([{'meta_tip_id': t.id} for t in fresh_tips])
+     
 
 @auth.requires_login()
 def fetch_tips():
     _add_defaut_tips()
-    #_add_new_tips()
+    _add_new_tips()
